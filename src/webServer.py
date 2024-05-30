f93e33c53add30f416ed837c96158fa53c9302ea
LJara92 <jara.lautaro@gmail.com>
2024-05-30 20:21:47 -0300

    Envio mail configurado

--- src/webServer.py
+++ src/webServer.py
@@ -1,24 +1,56 @@
-from flask import *
+from flask import Flask, request, render_template, flash, redirect, url_for
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
-#Rutas 
+# Configuración de Flask-Mail
+app.config['MAIL_SERVER'] = 'smtp-mail.outlook.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'infranetworks@hotmail.com'  
+app.config['MAIL_PASSWORD'] = '' #Clave mail
+app.config['SECRET_KEY'] = '' #Secret Key
+
+mail = Mail(app)
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/inicio')
-def inico():
+def inicio():
     return render_template("index.html")
 
 @app.route('/nosotros')
 def nosotros():
     return render_template("nosotros.html")
     
-@app.route('/contacto')
+@app.route('/contacto', methods=['GET', 'POST'])
 def contacto():
-    return render_template("contacto.html")
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        company = request.form['company']
+        preferred_contact_time = request.form['preferred_contact_time']
+        message = request.form['message']
+        
+        msg = Message(f'*** Contacto "{name}" ***', 
+                      sender='infranetworks@hotmail.com',
+                      recipients=['infranetworks@hotmail.com'])
+        msg.body = f"""
+        Nombre: {name}
+        E-mail: {email}
+        Teléfono: {phone}
+        Empresa: {company if company else 'No especificado'}
+        Horario de disponibilidad: {preferred_contact_time}
+        Mensaje: {message}
+        """
+        mail.send(msg)
+        flash('Su mensaje ha sido enviado. ¡Muchas gracias!')
+        return redirect(url_for('contacto'))
+    
+    return render_template('contacto.html')
 
-
-
-app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
